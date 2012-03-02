74926ea79d0d8815e2944bb54a4d11bdc20ab91c
Valentino <valentino@reenhanced.com>
2012-03-02 09:57:50 -0500

    Tried setting up multiple mock runners

--- spec/commands/command_helper.rb
+++ spec/commands/command_helper.rb
@@ -3,6 +3,11 @@ module CommandHelper
     Runner.new(self, cmd, *args, &block).run
   end
 
+  def running_twice(cmd, *args, &block)
+    @first_run = true
+    Runner.new(self, cmd, *args, &block).run_twice
+  end
+
   class Runner
     include SetupMethods
 
@@ -25,6 +30,37 @@ module CommandHelper
       args = @args.clone
       GitHub.parse_options(args) # strip out the flags
       GitHub.should_receive(:invoke).with(@cmd_name, *args).and_return do
+        GitHub.send(GitHub.send(:__mock_proxy).send(:munge, :invoke), @cmd_name, *args)
+      end
+      invoke = lambda { GitHub.activate([@cmd_name, *@args]) }
+      if @expected_result
+        expectation, result = @expected_result
+        case result
+        when Spec::Matchers::RaiseException, Spec::Matchers::Change, Spec::Matchers::ThrowSymbol
+          invoke.send expectation, result
+        else
+          invoke.call.send expectation, result
+        end
+      else
+        invoke.call
+      end
+      @stdout_mock.invoke unless @stdout_mock.nil?
+      @stderr_mock.invoke unless @stderr_mock.nil?
+    end
+
+    def run_twice
+      2.times do
+        self.instance_eval &@block
+        @first_run = false
+      end
+      mock_remotes unless @remotes.nil?
+      GitHub.should_receive(:load).twice.with(GitHub::BasePath + "/commands/commands.rb")
+      GitHub.should_receive(:load).twice.with(GitHub::BasePath + "/commands/helpers.rb")
+      GitHub.should_receive(:load).twice.with(GitHub::BasePath + "/commands/network.rb")
+      GitHub.should_receive(:load).twice.with(GitHub::BasePath + "/commands/issues.rb")
+      args = @args.clone
+      GitHub.parse_options(args) # strip out the flags
+      GitHub.should_receive(:invoke).twice.with(@cmd_name, *args).and_return do
         GitHub.send(GitHub.send(:__mock_proxy).send(:munge, :invoke), @cmd_name, *args)
       end
       invoke = lambda { GitHub.activate([@cmd_name, *@args]) }
--- spec/commands/command_pull-request_spec.rb
+++ spec/commands/command_pull-request_spec.rb
@@ -24,18 +24,16 @@ describe "github pull-request" do
   end
 
   specify "pull-request user should notify if pull request already exists" do
-    running :'pull-request', "user" do
-      setup_github_token
-      setup_url_for "origin", "user", "github-gem"
-      setup_remote "origin", :user => "user", :project => "github-gem"
-      setup_user_and_branch "user", "branch"
-      @helper.should_receive(:get_first_commit_message)
-      @command.should_receive(:sh).with("curl -F 'login=drnic' -F 'token=MY_GITHUB_TOKEN' -F \"pull[base]=master\" -F \"pull[head]=user:branch\" -F \"pull[title]=Commit\" -F \"pull[body]=Commit\" https://github.com/api/v2/json/pulls/user/github-gem")
-      running :'pull-request', "user" do
-        @helper.should_receive(:get_first_commit_message)
-        @command.should_receive(:sh).with("curl -F 'login=drnic' -F 'token=MY_GITHUB_TOKEN' -F \"pull[base]=master\" -F \"pull[head]=user:branch\" -F \"pull[title]=Commit\" -F \"pull[body]=Commit\" https://github.com/api/v2/json/pulls/user/github-gem")
-        @command.stdout.should == "A pull request already exists for user:branch"
+    running_twice :'pull-request', "user" do
+      if @first_run
+        setup_github_token
+        setup_url_for "origin", "user", "github-gem"
+        setup_remote "origin", :user => "user", :project => "github-gem"
+        setup_user_and_branch "user", "branch"
       end
+      @helper.should_receive(:get_first_commit_message).twice
+      #@command.should_receive(:sh).with("curl -F 'login=drnic' -F 'token=MY_GITHUB_TOKEN' -F \"pull[base]=master\" -F \"pull[head]=user:branch\" -F \"pull[title]=Commit\" -F \"pull[body]=Commit\" https://github.com/api/v2/json/pulls/user/github-gem")
+      stdout.should == "Error: A pull request already exists for user:branch"
     end
   end
 
